913687661ed67e319896a62c1fe5bb0963f6a813
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-09-19 19:29:15 +0600

    menu card image fix, functions added for themes in js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*==================== DARK LIGHT THEME ====================*/ 
+/*------------- DARK LIGHT THEME ----------- */ 
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'bx-sun'
@@ -7,13 +7,13 @@ const iconTheme = 'bx-sun'
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
+// obtain current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
 
-// We validate if the user previously chose a topic
+// validate if the user previously chose a topic
 if (selectedTheme) {
-// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+// If the validation is fulfilled, ask what the issue was to know if theme is activated or deactivated the dark
 document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
 themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
 }
